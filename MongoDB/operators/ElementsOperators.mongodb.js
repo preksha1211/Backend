223b9e560214b50b3051c0d72ab4b27d116d96ff
Preksha Gupta <prekshag460@gmail.com>
2026-07-31 06:03:39 +0530

    adding evalution operators

--- MongoDB/operators/ElementsOperators.mongodb.js
+++ MongoDB/operators/ElementsOperators.mongodb.js
@@ -1,14 +1,14 @@
-use ('myuser')
+use('myusers')
 
 // db.users.insertMany([
-//     {name:'mkl',age:21},{name:'manas'},{age:33},{name:'rohan',age:21},{name:'sohan',age:22},{name:'puneet',age:33}
+//     { name: 'mkl', age: 21 },
+//     { name: 'manas' },
+//     { age: 33 },
+//     { name: 'rohan', age: 21 },
+//     { name: 'sohan', age: '22' },
+//     { name: 'puneet', age: 33 },
 // ])
 
-// db.users.find({age:{$exists:false}});
+// db.users.find({name:{$exists:true}})
 
-
-db.users.find({
-  age: {
-    $type: "string"
-  }
-});
+// db.users.find({age:{$type:'string'}})
