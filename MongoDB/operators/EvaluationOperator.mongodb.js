6d9835e9d2a3a841fd9ec3a542c05b487f96ab33
Preksha Gupta <prekshag460@gmail.com>
2026-08-07 23:59:34 +0530

    ..

---
+++ MongoDB/operators/EvaluationOperator.mongodb.js
@@ -0,0 +1,20 @@
+use ('practiceDB')
+
+//regex
+
+// db.farms.find({
+//   name: {$regex: "Farm$"}
+// })
+
+// db.farms.find({
+//   name: { $regex: "river", $options: "i" }
+// });
+
+//exp
+
+// db.farms.find({$expr:{$gt:["$income",50000]}})
+
+db.farms.find({
+  $expr: { $gt: ["$income", "$area"] }
+});
+db.farms.find();
